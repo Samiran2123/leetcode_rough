614463d8d6c8f3f3f5b478790dc4e4a39f7da3ef
Samiran Das <dassamiran4368@gmail.com>
2026-07-15 23:38:44 +0530

    Majority element

---
+++ le169.java
@@ -0,0 +1,17 @@
+class le169 {
+    public int majorityElement(int[] arr) {
+       int ans = 0;
+       int count = 0;
+       for(int i = 0; i < arr.length; i++){
+        if(count == 0){
+            ans = arr[i];
+        }
+        if(ans==arr[i]){
+            count++;
+        }else{
+            count--;
+        }
+       }
+       return ans; 
+    }
+}
