fb3e532fdc52d0c30cf5ea61f80cf20f53764d9d
Samiran Das <dassamiran4368@gmail.com>
2026-07-21 23:47:54 +0530

    Power of Three

--- le3925.java
+++ le3925.java
@@ -1,24 +1,9 @@
 class le3925 {
     public int[] concatWithReverse(int[] nums) {
+      int arr []= new int [nums.length * 2];
+      
+       for(int i = 0; i < nums.length;i++){
 
-        int[] arr = new int[nums.length * 2];
-
-       
-        int numslast = nums.length - 1;
-        int arrlast = nums.length;
-
-        for(int i = 0; i < nums.length; i++){
-            arr[i] = nums[i];
-            arr[arrlast] = nums[numslast];
-
-            arrlast++;
-            numslast--;
-
-        }
-
-        return arr;
-
-        
-        
+       }
     }
 }
